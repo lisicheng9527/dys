8cdd26a42cbc3426026ed6d533db7c56d68cae69
lisicheng <lisicheng@tuhu.cn>
2024-11-25 02:49:36 +0800

    feat: 攒好运

---
+++ pages/chy/index.js
@@ -0,0 +1,16 @@
+Page({
+  data: {
+    categories: [
+      { name: "健康", value: 93, color: "#4caf50" },
+      { name: "财运", value: 73, color: "#ffc107" },
+      { name: "爱情", value: 83, color: "#ff5722" },
+      { name: "事业", value: 83, color: "#2196f3" },
+      { name: "学习", value: 83, color: "#795548" }
+    ],
+    coupons: [
+      { text: "美团 外卖红包", bgColor: "#ffe0b2" },
+      { text: "饿了么 外卖红包", bgColor: "#bbdefb" }
+    ],
+    value: 78.4, // 当前得分
+  }
+});
